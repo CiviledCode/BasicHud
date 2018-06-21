4cbb9fe8783aecf551bab951a1824bfcc6bc20ed
Civiled <civiledgamer@gmail.com>
2018-06-21 11:20:31 -0700

    Fixed null errors

--- src/main/java/com/rezolt/basichud/main.java
+++ src/main/java/com/rezolt/basichud/main.java
@@ -12,65 +12,50 @@ import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.TextFormat;
 import me.onebone.economyapi.EconomyAPI;
 
-public class main extends PluginBase implements Listener{
-	@Override
-	public void onEnable()
-	{
-		this.getLogger().info(TextFormat.GREEN + "Hud has been enabled!");
-		this.getServer().getPluginManager().registerEvents(this, this);
+public class main extends PluginBase implements Listener {
+    @Override
+    public void onEnable() {
+        this.getLogger().info(TextFormat.GREEN + "Hud has been enabled!");
+        this.getServer().getPluginManager().registerEvents(this, this);
         this.saveDefaultConfig();
-		
-	}
-	@Override
-	public void onDisable()
-	{
-		this.getLogger().info(TextFormat.RED + "Hud has been disabled...");
-		
-	}
-	Server server = this.getServer();
-	@EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
-	public void onMove(PlayerMoveEvent event)
-	{
-		Player player = event.getPlayer();
 
-  		DecimalFormat df = new DecimalFormat("#");
-		String x = df.format(player.getPosition().x);
-		String y = df.format(player.getPosition().y);
-		String z = df.format(player.getPosition().z);
-		String name = server.getName();
-		String msg = TextFormat.colorize(this.getConfig().getString("Format")
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info(TextFormat.RED + "Hud has been disabled...");
+
+    }
+
+    Server server = this.getServer();
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+
+        DecimalFormat df = new DecimalFormat("#");
+        String x = df.format(player.getPosition().x);
+        String y = df.format(player.getPosition().y);
+        String z = df.format(player.getPosition().z);
+        String msg = TextFormat.colorize(this.getConfig().getString("Format")
                 .replace("%x%", x)
                 .replace("%y%", y)
-        		.replace("%z%", z)
-        		.replace("%servername%", name)
-        		.replace("%playercount%", "")
-        		);
-		if(this.getServer().getPluginManager().getPlugin("EconomyAPI").isEnabled())
-		{
-			msg.replace("%money%", String.valueOf(EconomyAPI.getInstance().myMoney(player)));
-		}
-		else
-		{
-			this.getLogger().info(TextFormat.RED + "You cannot use the %money% var without EconomyAPI!");
-		}
-				
-				
-				
-				
-		String type = this.getConfig().getString("Type");
+                .replace("%z%", z)
+                .replace("%playercount%", "")
+        );
+        if(this.getServer().getPluginManager().getPlugin("EconomyAPI") != null)
+            msg = msg.replaceAll("%money%", String.valueOf(EconomyAPI.getInstance().myMoney(player)));
+        else
+            this.getLogger().info(TextFormat.RED + "You cannot use the %money% var without EconomyAPI!");
 
-		if(type.toLowerCase().contains("popup"))
-		{
-		player.sendPopup(msg);
-		}
-		else if(type.toLowerCase().contains("actionbar"))
-		{
-			player.sendActionBar(msg
-	                .replace("%x%", x)
-	                .replace("%y%", y)
-	        		.replace("%z%", z));
-			
-		}
-	}
+
+        String type = this.getConfig().getString("Type");
+
+        if(type.toLowerCase().contains("popup"))
+            player.sendPopup(msg);
+        else
+            player.sendActionBar(msg);
+
+    }
 
 }
